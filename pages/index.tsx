6505a63da4fcb09527179efb6f3e9dd0e674bbb6
Baticaly <ta6nbe@gmail.com>
2022-11-14 08:30:55 +0100

    dashboard view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,29 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Account from "../components/Account";
+import Dashboard from "./dashboard";
 
 const Home = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
 
   return (
-    <div className="container" style={{ padding: "50px 0 100px 0" }}>
-      <div className="loginCard">
-        <h1>gradooPanel</h1>
-      {!session ? (
-        <Auth
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          theme="dark"
-        />
-      ) : (
-        <Account session={session} />
-      )}
+    <>
+      <div className="container">
+        <div className="loginCard">
+          {!session ? (
+            <Auth
+              view="sign_in"
+              supabaseClient={supabase}
+              appearance={{ theme: ThemeSupa }}
+              theme="dark"
+            />
+          ) : (
+            <Dashboard session={session} />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
