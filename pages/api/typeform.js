e45d7e8554a5324121c46776e83ccd3e6e532456
Baticaly <ta6nbe@gmail.com>
2022-12-10 00:10:53 +0100

    full text search webhok

--- pages/api/typeform.js
+++ pages/api/typeform.js
@@ -1,10 +1,12 @@
 import { buffer } from 'micro'
+import { createClient } from '@supabase/supabase-js'
 const crypto = require("crypto");
 
 export default async function webhookReceiver(req, res) {
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
     const rawBody = (await buffer(req)).toString()
-    const data = JSON.parse(rawBody)
+    const webhookdata = JSON.parse(rawBody)
 
     const webhookSecret = process.env.TYPEFORM_WEBHOOK_SECRET;
     const verifySignature = function (receivedSignature, payload) {
@@ -18,12 +20,107 @@ export default async function webhookReceiver(req, res) {
     const signature = req.headers["typeform-signature"];
     const isValid = verifySignature(signature, rawBody.toString());
     console.log("isvalid", isValid);
+
     if (!isValid) {
         throw new Error("Webhook signature is not valid, someone is faking this!");
     }
 
+    const form_response = webhookdata.form_response
+
+    var firstname = " ";
+    var lastname = " ";
+    var phone = " ";
+    var email = " ";
+    var usertype = " ";
+    var school = " ";
+    var submitted_at = " ";
+    var highschool_city = " ";
+    var highschool_class = " ";
+    var university_program = " ";
+    var university_class = " ";
+
+    var eventID = form_response.definition.title;
+    submitted_at = form_response.submitted_at;
+    firstname = form_response.answers[0].text;
+    lastname = form_response.answers[1].text;
+
+    form_response.answers.forEach((answer) => {
+        switch (answer.field.ref) {
+            case "usertype":
+                usertype = answer.choice.label;
+                break;
+            case "highschool-name":
+                school = answer.text;
+                break;
+            case "highschool-city":
+                highschool_city = answer.choice.label;
+                break;
+            case "highschool-class":
+                highschool_class = answer.choice.label;
+                break;
+            case "university-name":
+                school = answer.choice.label;
+                break;
+            case "university-program":
+                university_program = answer.text;
+                break;
+            case "university-class":
+                university_class = answer.choice.label;
+                break;
+        }
+        if (answer.field.type === "phone_number") phone = answer.phone_number;
+        if (answer.field.type === "email") email = answer.email;
+    });
+
+    let searchstring = ''
+
+    if (school != " ") {
+        let splitted = ''
+        if (JSON.stringify(school).includes(' ')) {
+            splitted = JSON.stringify(school).replaceAll(' ', ' & ')
+            searchstring = searchstring.concat(splitted)
+        } else { searchstring = searchstring.concat(JSON.stringify(school)) }
+    }
+
+    if (highschool_city.length > 1) {
+        searchstring = searchstring.concat(' & ', highschool_city)
+    }
+
+    const { data } = await supabase.rpc('indexschool', { input: searchstring })
+
+    let citystring = ''
+    let schoolquery = ''
+
+    if (data !== null) {
+        if (data.length > 0) {
+            schoolquery = data[0].name
+            citystring = citystring.concat(data[0].city)
+        } else {
+            schoolquery = school
+            citystring = citystring.concat(highschool_city)
+        }
+    } else {
+        schoolquery = school
+        citystring = citystring.concat(highschool_city)
+    }
+
+    console.log('string - ', searchstring, 'data - ', data)
+
+    const { error } = await supabase
+        .from('applications')
+        .insert({
+            firstname: firstname,
+            lastname: lastname,
+            phone: phone,
+            email: email,
+            usertype: usertype,
+            school: schoolquery,
+            event: event,
+            submitted_at: submitted_at,
+            city: citystring
+        })
+
     res.status(200)
-    console.log(data)
 }
 
 export const config = {
