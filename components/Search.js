9eb7309483ec46978475d47029fccf893b1eb8d4
Baticaly <ta6nbe@gmail.com>
2022-12-14 21:08:30 +0100

    city filter update

--- components/Search.js
+++ components/Search.js
@@ -91,9 +91,22 @@ export default function Search(session) {
     ]
 
     const cityoptions = [
-        { value: 'Istanbul', label: 'Istanbul' },
-        { value: 'Ankara', label: 'Ankara' },
-        { value: 'Izmir', label: 'Izmir' },
+        { value: 'Adana', label: 'Adana' }, { value: 'Adıyaman', label: 'Adıyaman' }, { value: 'Afyon', label: 'Afyon' }, { value: 'Ağrı', label: 'Ağrı' }, { value: 'Amasya', label: 'Amasya' },
+        { value: 'Ankara', label: 'Ankara' }, { value: 'Antalya', label: 'Antalya' }, { value: 'Artvin', label: 'Artvin' }, { value: 'Aydın', label: 'Aydın' }, { value: 'Balıkesir', label: 'Balıkesir' },
+        { value: 'Bilecik', label: 'Bilecik' }, { value: 'Bingöl', label: 'Bingöl' }, { value: 'Bitlis', label: 'Bitlis' }, { value: 'Bolu', label: 'Bolu' }, { value: 'Burdur', label: 'Burdur' },
+        { value: 'Bursa', label: 'Bursa' }, { value: 'Çanakkale', label: 'Çanakkale' }, { value: 'Çankırı', label: 'Çankırı' }, { value: 'Çorum', label: 'Çorum' }, { value: 'Denizli', label: 'Denizli' },
+        { value: 'Diyarbakır', label: 'Diyarbakır' }, { value: 'Edirne', label: 'Edirne' }, { value: 'Elâzığ', label: 'Elâzığ' }, { value: 'Erzincan', label: 'Erzincan' }, { value: 'Erzurum', label: 'Erzurum' },
+        { value: 'Eskişehir', label: 'Eskişehir' }, { value: 'Gaziantep', label: 'Gaziantep' }, { value: 'Giresun', label: 'Giresun' }, { value: 'Gümüşhane', label: 'Gümüşhane' }, { value: 'Hakkâri', label: 'Hakkâri' },
+        { value: 'Hatay', label: 'Hatay' }, { value: 'Isparta', label: 'Isparta' }, { value: 'Mersin', label: 'Mersin' }, { value: 'İstanbul', label: 'İstanbul' }, { value: 'İzmir', label: 'İzmir' },
+        { value: 'Kars', label: 'Kars' }, { value: 'Kastamonu', label: 'Kastamonu' }, { value: 'Kayseri', label: 'Kayseri' }, { value: 'Kırklareli', label: 'Kırklareli' }, { value: 'Kırşehir', label: 'Kırşehir' },
+        { value: 'Kocaeli', label: 'Kocaeli' }, { value: 'Konya', label: 'Konya' }, { value: 'Kütahya', label: 'Kütahya' }, { value: 'Malatya', label: 'Malatya' }, { value: 'Manisa', label: 'Manisa' },
+        { value: 'Kahramanmaraş', label: 'Kahramanmaraş' }, { value: 'Mardin', label: 'Mardin' }, { value: 'Muğla', label: 'Muğla' }, { value: 'Muş', label: 'Muş' }, { value: 'Nevşehir', label: 'Nevşehir' },
+        { value: 'Niğde', label: 'Niğde' }, { value: 'Ordu', label: 'Ordu' }, { value: 'Rize', label: 'Rize' }, { value: 'Sakarya', label: 'Sakarya' }, { value: 'Samsun', label: 'Samsun' },
+        { value: 'Siirt', label: 'Siirt' }, { value: 'Sinop', label: 'Sinop' }, { value: 'Sivas', label: 'Sivas' }, { value: 'Tekirdağ', label: 'Tekirdağ' }, { value: 'Tokat', label: 'Tokat' },
+        { value: 'Trabzon', label: 'Trabzon' }, { value: 'Tunceli', label: 'Tunceli' }, { value: 'Şanlıurfa', label: 'Şanlıurfa' }, { value: 'Uşak', label: 'Uşak' }, { value: 'Van', label: 'Van' },
+        { value: 'Yozgat', label: 'Yozgat' }, { value: 'Zonguldak', label: 'Zonguldak' }, { value: 'Aksaray', label: 'Aksaray' }, { value: 'Bayburt', label: 'Bayburt' }, { value: 'Karaman', label: 'Karaman' },
+        { value: 'Kırıkkale', label: 'Kırıkkale' }, { value: 'Batman', label: 'Batman' }, { value: 'Şırnak', label: 'Şırnak' }, { value: 'Bartın', label: 'Bartın' }, { value: 'Ardahan', label: 'Ardahan' },
+        { value: 'Iğdır', label: 'Iğdır' }, { value: 'Yalova', label: 'Yalova' }, { value: 'Karabük', label: 'Karabük' }, { value: 'Kilis', label: 'Kilis' }, { value: 'Osmaniye', label: 'Osmaniye' }, { value: 'Düzce', label: 'Düzce' },
     ]
 
     const classoptions = [
@@ -374,7 +387,7 @@ export default function Search(session) {
                         </div>
 
                         <div className="flex grow basis-10">
-                            <CreatableSelect isClearable isMulti styles={colourStyles} placeholder={'City'} options={cityoptions} onChange={citychange} />
+                            <Select isClearable isMulti styles={colourStyles} placeholder={'City'} options={cityoptions} onChange={citychange} />
                         </div>
 
                         <div className="flex grow basis-10">
