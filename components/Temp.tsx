4d5b6a0689d07ddf10ea10f02089b11fa860eb04
Baticaly <ta6nbe@gmail.com>
2023-01-06 02:42:22 +0300

    time placeholder update

--- components/Temp.tsx
+++ components/Temp.tsx
@@ -1,12 +1,10 @@
 export default function Temp(dateValue) {
-    return (
-            <>
-                <div>
-                    {dateValue.startDate}
-                </div>
-                <div>
-                    {dateValue.endDate}
-                </div>
-            </>
-        );
+  return (
+    <>
+      <div>
+        {" "}
+        {dateValue.startDate} - {dateValue.endDate}{" "}
+      </div>
+    </>
+  );
 }
